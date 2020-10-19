7f47f19cfbac94584d4d6172672eef74a7e4b02c
rymdhund <7973243+rymdhund@users.noreply.github.com>
2020-10-19 16:58:28 +0000

    Fix cert verification

--- internal/chk/checker/cert.go
+++ internal/chk/checker/cert.go
@@ -59,18 +59,21 @@ func (c CertChecker) expiresSoonDaysOrDefault() int {
 
 func (c CertChecker) Run() base.Result {
 	addr := fmt.Sprintf("%s:%d", c.Host, c.portOrDefault())
-	conn, err := tls.Dial("tcp", addr, &tls.Config{
-		InsecureSkipVerify: true,
-	})
+	conn, err := tls.Dial("tcp", addr, &tls.Config{})
+	if err != nil {
+		return base.FailResult(niceTlsError(err))
+	}
+	defer conn.Close()
+
+	err = conn.VerifyHostname(c.Host)
 	if err != nil {
 		return base.FailResult(err.Error())
 	}
-	defer conn.Close()
 
 	errorMsg := "No certificate found"
 	for _, cert := range conn.ConnectionState().PeerCertificates {
 		if !cert.IsCA {
-			ok, msg := c.verifyCert(cert, time.Now())
+			ok, msg := c.verifyExpiry(cert, time.Now())
 			if ok {
 				return base.GoodResult()
 			}
@@ -80,33 +83,23 @@ func (c CertChecker) Run() base.Result {
 	return base.FailResult(errorMsg)
 }
 
-func (c CertChecker) verifyCert(crt *x509.Certificate, now time.Time) (bool, string) {
-	opts := x509.VerifyOptions{
-		DNSName:     c.Host,
-		CurrentTime: now,
+func (c CertChecker) verifyExpiry(crt *x509.Certificate, now time.Time) (bool, string) {
+	toExpiry := crt.NotAfter.Sub(now)
+	if toExpiry < time.Duration(c.expiresSoonDaysOrDefault()*24)*time.Hour {
+		return false, fmt.Sprintf("Cert expires in %d days", int(toExpiry.Hours()/24))
 	}
-	_, err := crt.Verify(opts)
-	if err == nil {
-		toExpiry := now.Sub(crt.NotAfter)
-		if toExpiry < time.Duration(c.expiresSoonDaysOrDefault()*24)*time.Hour {
-			return false, fmt.Sprintf("Cert expires in %d days", int(toExpiry.Hours()/24))
-		}
+	return true, ""
+}
 
-		return true, ""
-	}
-
-	err1, ok := err.(x509.CertificateInvalidError)
-	if ok {
-		switch err1.Reason {
-		case x509.Expired:
-			return false, "Cert expired or not yet valid"
-		}
-	}
-
-	switch err.(type) {
+func niceTlsError(err error) string {
+	switch e := err.(type) {
 	case x509.UnknownAuthorityError:
-		return false, "Cert not signed by trusted authority"
+		return "Cert not signed by trusted authority"
+	case x509.CertificateInvalidError:
+		switch e.Reason {
+		case x509.Expired:
+			return "Cert expired or not yet valid"
+		}
 	}
-
-	return false, err.Error()
+	return err.Error()
 }
